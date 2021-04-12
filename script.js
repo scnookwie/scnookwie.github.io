78999f2e805665c990ce50b2be22236368294468
scnookwie <80013469+scnookwie@users.noreply.github.com>
2021-04-12 10:19:11 +0800

    11

---
+++ script.js
@@ -0,0 +1,15 @@
+function sayHello(){
+  var response = prompt("What's your name");
+   alert("Hello " + response + ", let's have a puzzle game!");
+ }
+function add(a,b){
+  return a+b;
+ }
+ console.log(add(20,30));
+ console.log(add(120,130));
+
+ function showTopic(){
+   var x = document.getElementById('demo');
+   x.style.fontSize="25px";
+   x.style.color="red";
+ }
