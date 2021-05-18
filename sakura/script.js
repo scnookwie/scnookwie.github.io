ef7f308e9f66fcd1d8fbb00988dcc185c7e28c30
Scnookwie <332518524@qq.com>
2021-05-18 23:10:21 +0800

    123
    123

--- sakura/script.js
+++
@@ -1,9 +0,0 @@
-function sayAnswer(){
-  var response = prompt("What's your answer");
-  if( response==="P"){
-  alert("Congratulations!You're right.");
- }
- else{
-  alert("Sorry,please try again.");
-}
-};
